6635c9fd8e7efef3c87d137e3da10fdfcc66aa49
nikolastasic <nklstasic93@gmail.com>
2024-03-09 21:45:42 +0100

    Fixes

--- cypress/e2e/1-signup/signup.cy.js
+++ cypress/e2e/1-signup/signup.cy.js
@@ -1,11 +1,10 @@
 import { faker } from "@faker-js/faker";
+import testData from "../../fixtures/testData.json";
 
 let email = faker.internet.email();
 let password = faker.internet.password();
 let firstName = faker.person.firstName();
 let lastName = faker.person.firstName();
-let country = "Serbia";
-let mobileNumber = "+381111222333";
 
 describe("Signup tests", () => {
   context("Front-end signup tests", () => {
@@ -17,16 +16,18 @@ describe("Signup tests", () => {
     it("Successfull signup", () => {
       cy.clickOnSignUp();
       cy.signUp(email, password);
-      cy.log("Title" + cy.getTitle());
-      cy.selectTitle("Mr.");
+      cy.selectTitle(testData.title);
       cy.enterName(firstName, lastName);
       cy.clickOnSubmitButton();
-      cy.clickOnElementContainsText("Press");
-      cy.clickOnElementContainsText("Co-ownership");
-      cy.clickOnElementContainsText("CHF");
+      cy.clickOnElementContainsText(testData.discoverFoxstone);
+      cy.clickOnElementContainsText(testData.productInterestedIn);
+      cy.clickOnElementContainsText(testData.availableCapital);
       cy.clickOnSubmitButton();
-      cy.clickOnElementContainsText("Now");
-      cy.selectCountryAndEnterMobileNumber(country, mobileNumber);
+      cy.clickOnElementContainsText(testData.firstInvestmentTime);
+      cy.selectCountryAndEnterMobileNumber(
+        testData.country,
+        testData.mobileNumber
+      );
       cy.interceptVerificationPin();
       cy.waitPageToLoad();
       cy.location("pathname").should("equal", "/en/thank-you");
