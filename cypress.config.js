0798f7b27651edae1def9e12aaa12dfe6214d62b
nikolastasic <nklstasic93@gmail.com>
2024-03-11 18:31:13 +0100

    Adding cypress record key

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "rmn4p6",
+  projectId: "rnzhqd",
   env: {
     joinUrl: "https://legacy-api.foxstone.co/api/join?lang=en",
     loginUrl:
